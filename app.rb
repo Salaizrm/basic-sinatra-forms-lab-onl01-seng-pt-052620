9df4c52434479f607b2cbd335c78d957a1d95bc1
Robert Salaiz <work.salaizrm@gmail.com>
2020-08-20 22:46:28 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -7,6 +7,13 @@ class App < Sinatra::Base
   end
 
   post '/team' do
+    @team_name = "name"
+    @coach = "coach"
+    @point_guard = "pg"
+    @shooting_guard = "sg"
+    @small_forward = "sf"
+    @power_forward = "pf"
+    @center = "c"
     erb :team
   end
 
